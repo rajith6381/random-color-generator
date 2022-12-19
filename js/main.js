289c6f958f0ae8dedd21732deeb7062d22c58208
rajith6381 <balarajith@gmail.com>
2022-12-19 12:13:12 +0530

    add file

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const getColor=()=>{
+    const randomNumber=Math.floor(Math.random()*16777215)
+    const randomCode="#"+randomNumber.toString(16);
+    console.log(randomNumber,randomCode);
+    document.body.style.backgroundColor=randomCode;
+    document.getElementById("color-code").innerHTML=randomCode;
+
+
+}
+document.getElementById("btn").addEventListener("click",getColor);
